40c5cef08b1f534e5e4e0b6c767f27310fdfbdec
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2024-05-26 14:33:27 -0700

    lispdot

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -280,3 +280,12 @@ vim.diagnostic.config({
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+vim.filetype.add({
+	extension = {
+		lspgd = "clojure",
+		lispdot = "clojure",
+		gdlisp = "clojure",
+		lispgd = "clojure",
+	},
+})
