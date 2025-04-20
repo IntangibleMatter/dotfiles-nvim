bd1f94eebb8ee53224a6d7d3bbe26c363ba15a0b
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2025-04-19 19:47:02 -0700

    configure hex

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -371,7 +371,29 @@ return {
 	},
 
 	-- hex editor
-	{ "RaafatTurki/hex.nvim" },
+	{
+		"RaafatTurki/hex.nvim",
+		opts = {
+
+			-- cli command used to dump hex data
+			dump_cmd = "xxd -g 1 -u",
+
+			-- cli command used to assemble from hex data
+			assemble_cmd = "xxd -r",
+
+			-- function that runs on BufReadPre to determine if it's binary or not
+			is_file_binary_pre_read = function()
+				-- logic that determines if a buffer contains binary data or not
+				-- must return a bool
+			end,
+
+			-- function that runs on BufReadPost to determine if it's binary or not
+			is_file_binary_post_read = function()
+				-- logic that determines if a buffer contains binary data or not
+				-- must return a bool
+			end,
+		},
+	},
 
 	-- utils
 	-- a lot of stuff seems to use this
