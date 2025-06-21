6d0be9a385d7109b57dc2abe5eb84a5e42955110
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2025-06-20 18:45:44 -0700

    aksdjh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -401,6 +401,18 @@ return {
 		end,
 		ft = { "markdown" },
 	},
+
+	-- HTML and the like
+	{
+		"brianhuster/live-preview.nvim",
+		dependencies = {
+			-- You can choose one of the following pickers
+			"nvim-telescope/telescope.nvim",
+			"ibhagwan/fzf-lua",
+			"echasnovski/mini.pick",
+		},
+	},
+
 	-- JSON
 	-- json schema support
 	{ "b0o/schemastore.nvim" },
