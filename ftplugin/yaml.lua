662f051b49cd45814020f096929bde55567df72b
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2025-10-12 16:24:50 -0700

    Yaml

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,7 +1,8 @@
 --vim.cmd("setlocal
 
-vim.bo.tabstop = 4 -- number of visual spaces per TAB
-vim.bo.softtabstop = 4 -- number of spacesin tab when editing
-vim.bo.shiftwidth = 4 -- insert 4 spaces on a tab
---vim.bo.expandtab=false -- tabs are spaces, mainly because of python
+print("WHYHYYYY")
+vim.bo.tabstop = 2 -- number of visual spaces per TAB
+vim.bo.softtabstop = 2 -- number of spacesin tab when editing
+vim.bo.shiftwidth = 2 -- insert 4 spaces on a tab
+vim.bo.expandtab = true -- tabs are spaces, mainly because of python
 --
