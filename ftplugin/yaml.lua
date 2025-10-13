945fa8b436efdefcb507614227545eb68c459a36
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2025-10-12 17:52:56 -0700

    remove yaml print at start lmao

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,6 +1,5 @@
 --vim.cmd("setlocal
 
-print("WHYHYYYY")
 vim.bo.tabstop = 2 -- number of visual spaces per TAB
 vim.bo.softtabstop = 2 -- number of spacesin tab when editing
 vim.bo.shiftwidth = 2 -- insert 4 spaces on a tab
